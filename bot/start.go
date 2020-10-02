8c58dbb083b1c4e682bf093febd62ac2682383ef
Matthew Trip <matthewtrip2@gmail.com>
2020-10-02 15:40:54 +0100

    Temp Image Check

--- bot/start.go
+++ bot/start.go
@@ -7,6 +7,7 @@ import (
 	"github.com/bwmarrin/discordgo"
 	"os"
 	"os/signal"
+	"regexp"
 	"syscall"
 )
 
@@ -31,10 +32,34 @@ func Start() {
 		return
 	}
 
+	session.AddHandler(imagesOnly)
+
 	fmt.Println("Bot is now running. Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
 	_ = session.Close()
+}
+
+func imagesOnly(s *discordgo.Session, m *discordgo.Message) {
+
+	// Check if the message came from the Nino channel.
+	if m.ChannelID != "760935127303192616" {
+		return
+	}
+
+	// Check if the message has an image url. If so, it's fine.
+	match, _ := regexp.MatchString("(http(s?):)([/|.|\\w|\\s|-])*\\.(?:jpg|gif|png)", m.Content)
+	if match {
+		return
+	}
+
+	// Otherwise, check if there are attachments. If so, it's also fine.
+	if len(m.Attachments) > 0 {
+		return
+	}
+
+	// But otherwise, delete the message because we don't want it.
+	_ = s.ChannelMessageDelete(m.ChannelID, m.ID)
 }
