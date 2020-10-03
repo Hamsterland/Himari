6e8ea40d182ef7c79e586c7db9b2fd746cec2bde
Matthew Trip <matthewtrip2@gmail.com>
2020-10-03 03:01:47 +0100

    nhinfo Improvements

--- commands/nhinfo.go
+++ commands/nhinfo.go
@@ -16,6 +16,7 @@ func nhentaiInfoCommand(ctx *exrouter.Context) {
 	c, _ := ctx.Channel(ctx.Msg.ChannelID)
 	if !c.NSFW {
 		_, _ = ctx.Reply(fmt.Sprintf("%s this command can only be used in an NSFW chnanel.", author.Mention()))
+		return
 	}
 
 	if len(args) == 0 {
@@ -48,13 +49,39 @@ func nhentaiInfoCommand(ctx *exrouter.Context) {
 		title = book.Titles.English
 	} else if book.Titles.Japanese != "" {
 		title = book.Titles.Japanese
+	} else {
+		title = "Himari Error: Title Not Found (Chinese and Korean Titles Unsupported)"
 	}
 
 	cover, _ := book.GetCoverUrl()
 
 	var tags []string
+	var artists []string
+	var characters []string
+	var languages []string
+	var groups []string
+	var categories []string
+
 	for _, tag := range book.Tags {
-		tags = append(tags, tag.Name)
+
+		// Get the markdown link
+		link := fmt.Sprintf("[%s](https://nhentai.net%s)", tag.Name, tag.Url)
+
+		// Find specific information.
+		switch tag.Type {
+		case "artist":
+			artists = append(artists, link)
+		case "character":
+			characters = append(characters, link)
+		case "language":
+			languages = append(languages, link)
+		case "group":
+			groups = append(groups, link)
+		case "category":
+			categories = append(categories, link)
+		default:
+			tags = append(tags, link)
+		}
 	}
 
 	embed := NewEmbed().
@@ -63,17 +90,33 @@ func nhentaiInfoCommand(ctx *exrouter.Context) {
 		SetImage(cover)
 
 	if book.Scanlator != "" {
-		embed.AddField("Scanlator", book.Scanlator, false)
+		embed.AddField("Scanlator", book.Scanlator, true)
 	}
 
-	if len(book.Tags) > 0 {
-		embed.AddField("Tags", strings.Join(tags, ", "), false)
+	if len(tags) > 0 {
+		embed.AddField("Tags", strings.Join(tags, ", "), true)
+	}
+
+	if len(characters) > 0 {
+		embed.AddField("Characters", strings.Join(characters, ", "), true)
+	}
+
+	if len(languages) > 0 {
+		embed.AddField("Languages", strings.Join(languages, ", "), true)
+	}
+
+	if len(groups) > 0 {
+		embed.AddField("Groups", strings.Join(groups, ", "), true)
+	}
+
+	if len(categories) > 0 {
+		embed.AddField("Categories", strings.Join(groups, ", "), true)
 	}
 
 	embed.
 		AddField("Uploaded", book.GetUtcUploadDate().Format(time.RFC850), true).
 		AddField("Favourites", fmt.Sprintf("%d", book.TotalFavorites), true).
-		AddField("Pages", fmt.Sprintf("%d", book.GetPageCount()), true)
+		AddField("Pages", fmt.Sprintf("%d", book.GetPageCount()), false)
 
 	_, _ = ctx.Ses.ChannelMessageSendEmbed(ctx.Msg.ChannelID, embed.MessageEmbed)
 }
